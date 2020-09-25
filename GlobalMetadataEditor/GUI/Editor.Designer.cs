7c89495fa2a5a05ef9e84acebbb6fe7c0fd21dd6
qintao <chenqintao@salmonads.com>
2020-09-25 16:45:48 +0800

    coding

--- GlobalMetadataEditor/GUI/Editor.Designer.cs
+++ GlobalMetadataEditor/GUI/Editor.Designer.cs
@@ -44,12 +44,16 @@ namespace GlobalMetadataEditor.GUI
             this.wholeWord = new System.Windows.Forms.CheckBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.editableListView = new GlobalMetadataEditor.GUI.EditableListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.original = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.replace = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.hasModify = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button2 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -78,28 +82,28 @@ namespace GlobalMetadataEditor.GUI
             // 打开文件ToolStripMenuItem
             // 
             this.打开文件ToolStripMenuItem.Name = "打开文件ToolStripMenuItem";
-            this.打开文件ToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.打开文件ToolStripMenuItem.Size = new System.Drawing.Size(144, 26);
             this.打开文件ToolStripMenuItem.Text = "打开文件";
             this.打开文件ToolStripMenuItem.Click += new System.EventHandler(this.OpenFile);
             // 
             // 保存文件ToolStripMenuItem
             // 
             this.保存文件ToolStripMenuItem.Name = "保存文件ToolStripMenuItem";
-            this.保存文件ToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.保存文件ToolStripMenuItem.Size = new System.Drawing.Size(144, 26);
             this.保存文件ToolStripMenuItem.Text = "保存文件";
             this.保存文件ToolStripMenuItem.Click += new System.EventHandler(this.SaveFile);
             // 
             // 另存文件ToolStripMenuItem
             // 
             this.另存文件ToolStripMenuItem.Name = "另存文件ToolStripMenuItem";
-            this.另存文件ToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.另存文件ToolStripMenuItem.Size = new System.Drawing.Size(144, 26);
             this.另存文件ToolStripMenuItem.Text = "另存文件";
             this.另存文件ToolStripMenuItem.Click += new System.EventHandler(this.SaveAs);
             // 
             // 关闭文件ToolStripMenuItem
             // 
             this.关闭文件ToolStripMenuItem.Name = "关闭文件ToolStripMenuItem";
-            this.关闭文件ToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.关闭文件ToolStripMenuItem.Size = new System.Drawing.Size(144, 26);
             this.关闭文件ToolStripMenuItem.Text = "关闭文件";
             this.关闭文件ToolStripMenuItem.Click += new System.EventHandler(this.CloseFile);
             // 
@@ -124,25 +128,25 @@ namespace GlobalMetadataEditor.GUI
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 25);
             this.button1.TabIndex = 2;
-            this.button1.Text = "搜索";
+            this.button1.Text = "下一个";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.OnButtonClick);
+            this.button1.Click += new System.EventHandler(this.OnNextButtonClick);
             // 
-            // checkBox1
+            // letterCase
             // 
             this.letterCase.AutoSize = true;
             this.letterCase.Location = new System.Drawing.Point(699, 35);
-            this.letterCase.Name = "checkBox1";
+            this.letterCase.Name = "letterCase";
             this.letterCase.Size = new System.Drawing.Size(74, 19);
             this.letterCase.TabIndex = 3;
             this.letterCase.Text = "大小写";
             this.letterCase.UseVisualStyleBackColor = true;
             // 
-            // checkBox2
+            // wholeWord
             // 
             this.wholeWord.AutoSize = true;
             this.wholeWord.Location = new System.Drawing.Point(785, 35);
-            this.wholeWord.Name = "checkBox2";
+            this.wholeWord.Name = "wholeWord";
             this.wholeWord.Size = new System.Drawing.Size(59, 19);
             this.wholeWord.TabIndex = 4;
             this.wholeWord.Text = "整词";
@@ -157,6 +161,23 @@ namespace GlobalMetadataEditor.GUI
             // 
             this.saveFileDialog1.FileName = "global-metadata.dat";
             // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 506);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(908, 25);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(84, 20);
+            this.toolStripStatusLabel1.Text = "字符串修改";
+            // 
             // editableListView
             // 
             this.editableListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
@@ -167,16 +188,13 @@ namespace GlobalMetadataEditor.GUI
             this.editableListView.FullRowSelect = true;
             this.editableListView.GridLines = true;
             this.editableListView.HideSelection = false;
-            this.editableListView.LabelEdit = true;
             this.editableListView.Location = new System.Drawing.Point(12, 62);
+            this.editableListView.MultiSelect = false;
             this.editableListView.Name = "editableListView";
             this.editableListView.Size = new System.Drawing.Size(884, 457);
             this.editableListView.TabIndex = 5;
             this.editableListView.UseCompatibleStateImageBehavior = false;
             this.editableListView.View = System.Windows.Forms.View.Details;
-            this.editableListView.LabelEdit = false;
-            this.editableListView.MultiSelect = false;
-
             // 
             // columnHeader1
             // 
@@ -198,12 +216,24 @@ namespace GlobalMetadataEditor.GUI
             this.hasModify.Text = "是否修改";
             this.hasModify.Width = 72;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(618, 60);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 25);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "上一个";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.OnPreButtonClick);
+            // 
             // Editor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(908, 531);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.editableListView);
             this.Controls.Add(this.wholeWord);
             this.Controls.Add(this.letterCase);
@@ -216,6 +246,8 @@ namespace GlobalMetadataEditor.GUI
             this.Text = "global-metadata Editor";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -242,5 +274,8 @@ namespace GlobalMetadataEditor.GUI
         private ColumnHeader columnHeader1;
         private OpenFileDialog openFileDialog1;
         private SaveFileDialog saveFileDialog1;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
+        private Button button2;
     }
 }
--- GlobalMetadataEditor/GUI/Editor.cs
+++ GlobalMetadataEditor/GUI/Editor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -14,14 +15,8 @@ namespace GlobalMetadataEditor.GUI
 {
     public partial class Editor : Form
     {
-        enum SearchMode
-        {
-            IgnoreCase,//忽略大小写，默认
-            Sensitive,//大小写
-            WholeWordIgnoreCase,//整词匹配，忽略大小写
-            WholeWordSensitive//整词大小写
-        }
         private MetadataFile file;
+        private string filePath;
         public Editor()
         {
             InitializeComponent();
@@ -34,16 +29,17 @@ namespace GlobalMetadataEditor.GUI
             //设置listview列尺寸
             int listViewWidth = this.Width - 40;
             this.editableListView.Width = listViewWidth;
-            this.editableListView.Height = this.Height - 100;
+            this.editableListView.Height = this.Height - 110;
             int columnSize = (this.editableListView.Width - 114) / 2;
             this.original.Width = columnSize;
             this.replace.Width = columnSize;
             //搜索栏尺寸
-            int textBoxWidth = listViewWidth - 185;
+            int textBoxWidth = listViewWidth - 225;
             this.textBox1.Width = textBoxWidth;
             this.button1.Location = new Point(textBoxWidth + 15, 25);
-            this.letterCase.Location = new Point(textBoxWidth + 80, 29);
-            this.wholeWord.Location = new Point(textBoxWidth + 150, 29);
+            this.button2.Location = new Point(textBoxWidth + 75, 25);
+            this.letterCase.Location = new Point(textBoxWidth + 140, 29);
+            this.wholeWord.Location = new Point(textBoxWidth + 200, 29);
             
         }
         protected override void OnSizeChanged(EventArgs e)
@@ -72,52 +68,63 @@ namespace GlobalMetadataEditor.GUI
         {
             if (openFileDialog1.ShowDialog(this) == DialogResult.OK)
             {
-                ThreadPool.QueueUserWorkItem(delegate {
-                    try
-                    {
-                        CloseFile(sender,e);
-                        file = new MetadataFile(openFileDialog1.FileName);
-                        Invoke(new Action<List<string>>(AddItemToListView), file.stringList);
-
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(ex);
-                        file?.Dispose();
-                        file = null;
-                    }
-                });
+                ReadDataToList(sender, e);
             }
+        }
+
+        private void ReadDataToList(object sender, EventArgs e)
+        {
+            ThreadPool.QueueUserWorkItem(delegate {
+                try
+                {
+                    CloseFile(sender, e);
+                    filePath = openFileDialog1.FileName;
+                    file = new MetadataFile(filePath);
+                    Invoke(new Action<List<string>>(AddItemToListView), file.stringList);
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                    file?.Dispose();
+                    file = null;
+                }
+            });
         }
 
         private void SaveFile(object sender, EventArgs e)
         {
+            if(file == null)
+            {
+                return;
+            }
+            MessageBoxButtons messageBoxButtons = MessageBoxButtons.OKCancel;
+            DialogResult result = MessageBox.Show("该操作无法撤回！", "注意！", messageBoxButtons);
+            if (result == DialogResult.OK)
+            {
+                List<byte[]> byteList = GetModifyByteList();
+                ThreadPool.QueueUserWorkItem(delegate
+                {
+                    string tempFile = filePath + ".temp";
+                    file.WriteToNewFile(tempFile, byteList);
+                    file?.Dispose();
+                    File.Delete(filePath);
+                    FileInfo info = new FileInfo(tempFile);
+                    info.MoveTo(filePath);
+                    ReadDataToList(sender, e);
+                });
+            }
+            
 
         }
 
         private void SaveAs(object sender, EventArgs e)
         {
-            List<byte[]> byteList = new List<byte[]>();
-            List<string> stringlist = new List<string>();
-            for (int i = 0; i < this.editableListView.Items.Count; i++)
+            if (file == null)
             {
-                string originaltext = this.editableListView.Items[i].SubItems[1].Text;
-                string replacetext = this.editableListView.Items[i].SubItems[2].Text;
-                //Console.WriteLine(originaltext);
-                //Console.WriteLine(replacetext);
-                if (replacetext.Equals(""))
-                {
-                    stringlist.Add(originaltext);
-                    byteList.Add(Encoding.UTF8.GetBytes(originaltext));
-                    continue;
-                }
-                byteList.Add(Encoding.UTF8.GetBytes(replacetext));
-                stringlist.Add(replacetext);
-                
+                return;
             }
-            foreach(string item in stringlist){
-                Console.WriteLine(item);
-            }
+            List<byte[]> byteList = GetModifyByteList();
             if (saveFileDialog1.ShowDialog(this) == DialogResult.OK)
             {
                 ThreadPool.QueueUserWorkItem(delegate
@@ -127,21 +134,41 @@ namespace GlobalMetadataEditor.GUI
             }
         }
 
+        private List<byte[]> GetModifyByteList()
+        {
+            List<byte[]> byteList = new List<byte[]>();
+            List<string> stringlist = new List<string>();
+            for (int i = 0; i < this.editableListView.Items.Count; i++)
+            {
+                string originaltext = this.editableListView.Items[i].SubItems[1].Text;
+                string replacetext = this.editableListView.Items[i].SubItems[2].Text;
+                if (replacetext.Equals(""))
+                {
+                    stringlist.Add(originaltext);
+                    byteList.Add(Encoding.UTF8.GetBytes(originaltext));
+                    continue;
+                }
+                byteList.Add(Encoding.UTF8.GetBytes(replacetext));
+                stringlist.Add(replacetext);
+
+            }
+            return byteList;
+        }
+
         private void CloseFile(object sender, EventArgs e)
         {
             file?.Dispose();
             Invoke(new Action(delegate { this.editableListView.Items.Clear(); }));
         }
 
-        private void OnButtonClick(object sender, EventArgs e)
+        private void OnNextButtonClick(object sender, EventArgs e)
         {
             string text = this.textBox1.Text;
-            //if(text == "")
-            //{
-            //    return;
-            //}
+            if (text == "")
+            {
+                return;
+            }
             int index = this.editableListView.SelectedIndices.Count > 0 ? this.editableListView.SelectedIndices[0] : -1;
-            Console.WriteLine(index);
             int listCount = this.editableListView.Items.Count;
             for (int i=0;i<listCount-index-1;i++)
             {
@@ -151,36 +178,59 @@ namespace GlobalMetadataEditor.GUI
                     this.editableListView.Focus();
                     item.Selected = true;
                     item.EnsureVisible();
+                    this.toolStripStatusLabel1.Text = String.Format("已找到字符串：{0}", text);
                     return;
                 }
             }
-            this.editableListView.Items[0].Selected = true;
-
-            
-
-
-
-
+            this.editableListView.SelectedIndices.Clear();
+            this.toolStripStatusLabel1.Text = String.Format("未能找到字符串：{0}，指针回到列表头部", text);
         }
 
-        private bool CompareString(string pattern,string text)
+        private bool CompareString(string value,string text)
         {
             if (!this.letterCase.Checked && !this.wholeWord.Checked)
             {
-                return false;
+                return text.ToLower().Contains(value.ToLower());
             }
             else if (this.letterCase.Checked && !this.wholeWord.Checked)
             {
-                return text.Contains(pattern);
+                return text.Contains(value);
             }
             else if (!this.letterCase.Checked && this.wholeWord.Checked)
             {
-                return false;
+                string pattern = String.Format(@"\b{0}\b", value);
+                return Regex.Match(text, pattern,RegexOptions.IgnoreCase).Length != 0;
             }
             else
             {
-                return false;
+                string pattern = String.Format(@"\b{0}\b", value);
+                return Regex.Match(text, pattern).Length != 0;
             }
+        }
+
+        private void OnPreButtonClick(object sender, EventArgs e)
+        {
+            string text = this.textBox1.Text;
+            if (text == "")
+            {
+                return;
+            }
+            int listCount = this.editableListView.Items.Count;
+            int index = this.editableListView.SelectedIndices.Count > 0 ? this.editableListView.SelectedIndices[0] : listCount;
+            for (int i = 0; i<index; i++)
+            {
+                var item = this.editableListView.Items[index-i-1];
+                if (CompareString(text, item.SubItems[1].Text))
+                {
+                    this.editableListView.Focus();
+                    item.Selected = true;
+                    item.EnsureVisible();
+                    this.toolStripStatusLabel1.Text = String.Format("已找到字符串：{0}", text);
+                    return;
+                }
+            }
+            this.editableListView.SelectedIndices.Clear();
+            this.toolStripStatusLabel1.Text = String.Format("未能找到字符串：{0}，指针回到列表头部", text);
         }
 
     }
